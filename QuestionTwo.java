59ad6a0d37face135aff00209c8cbaf16cd80f68
Alex <przeroone@gmail.com>
2022-01-27 22:31:25 +0800

    Dynamic generate the binary tree according to the depth inputted

--- QuestionTwo.java
+++ QuestionTwo.java
@@ -3,9 +3,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class QuestionTwo {
     private static void checkArguement(String[] args) {
-        Pattern pattern = Pattern.compile("^\\d{0,2}$");
+        Pattern pattern = Pattern.compile("^\\d{0,1}$");
         if (args.length == 0 || !pattern.matcher(args[0]).matches()) {
-            System.out.println("Invalid input. Please input a positive number from 0 to 99.");
+            System.out.println("Invalid input. Please input a positive number from 0 to 9.");
             System.exit(0);
         }
     } 
@@ -16,7 +16,7 @@ public class QuestionTwo {
         // Build binary tree
         BinaryTree bt = new BinaryTree();
         ThreadLocalRandom rand = ThreadLocalRandom.current();
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i < (Math.pow(2, n + 1) - 1); i++) {
             int v = rand.nextInt(1, 1000 + 1);
             bt.addNode(new Node(v));
         }
